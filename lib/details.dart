bf1eddd94fa655b4ad8a5a296963a4bbdff3cbca
swathi Krishna TS <swathikrishna2592@gmail.com>
2024-02-28 12:23:21 +0530

    network image

--- lib/details.dart
+++ lib/details.dart
@@ -2,21 +2,24 @@ import 'package:flutter/material.dart';
 
 class Details extends StatelessWidget {
   final String name;
-  
+
   final String message;
 
   final String image;
-  
-  
-  const Details({required this.name,required this.message,required this.image,super.key});
+
+  const Details(
+      {required this.name,
+      required this.message,
+      required this.image,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: CircleAvatar(),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(backgroundImage: AssetImage(image)),
         ),
         backgroundColor: Colors.teal.shade800,
         title: Text(
@@ -44,7 +47,34 @@ class Details extends StatelessWidget {
               ))
         ],
       ),
-      body: Container(height: 50,width: 50,color: Colors.purple,child: Text(message),),
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            width: 500,
+            color: Colors.purple,
+            child: Text(message),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: const Text("heloo world"),
+                height: 300,
+                width: 300,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&w=600"),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
